0359ac1a370d0dc443e4ad0577c0e497e6f9d455
haovan <minhhao97vn@gmail.com>
2019-12-07 16:49:35 -0600

    up

--- VCNN/js/index.js
+++ VCNN/js/index.js
@@ -31,10 +31,10 @@ function updateInputs() {
                 loadAllVariablesModel();
             } else {
                 // loadAllPretrainModelFromServer("new_arrTemperature0_100_process");
-                let num = "";
-                // loadAllPretrainModelFromServer("arrTemp0_ts100_e30_b8_lr0005_L8L8D8D4_"+num);
-                // loadAllPretrainModelFromServer("stock_ts4_e100_b8_lr0005_L8L8D8D4_" + num);
-                loadModelFromServer("emp_super_large_settings" + num);
+                let num = 2;
+                // loadModelFromServer("arrTemp0_ts100_e30_b8_lr0005_L8L8D8D4_"+num);
+                loadModelFromServer("stock_ts4_e100_b8_lr0005_L8L8D8D4_" + num);
+                // loadModelFromServer("emp_super_large_settings" + num);
                 // loadModelFromServer("stock_large_model_2" + num);
             }
             // loadAllPretrainModelFromServerV2(target_variable_V2);
@@ -180,8 +180,8 @@ async function processInputs(sFs) {
                 d3.json("data/newData/X_test_stock_ts4_fri.json").then(X_testR => {
                     d3.json("data/newData/y_test_stock_ts4_fri.json").then(y_testR => {
                         // features = ['CPU1 Temp', 'CPU2 Temp', 'Inlet Temp', 'CPU Load', 'Memory usage', 'Fan1 speed', 'Fan2 speed', 'Fan3 speed', 'Fan4 speed', 'Power consumption'];
-                        // features = ['Open', 'High', 'Low', 'Close', 'Volume'];
-                        features = ['Total_Nonfarm', 'Total_Private', 'Goods_Producing', 'Service_Providing', 'Manufacturing', 'Trade|Transportation|Utilities', 'Wholesale_Trade', 'Retail_Trade', 'Transportation|Warehousing|Utilities', 'Financial_Activities', 'Professional_and_Business_Services', 'Education|Health_Services', 'Leisure_and_Hospitality', 'Other_Services', 'Government'];
+                        features = ['Open', 'High', 'Low', 'Close', 'Volume'];
+                        // features = ['Total_Nonfarm', 'Total_Private', 'Goods_Producing', 'Service_Providing', 'Manufacturing', 'Trade|Transportation|Utilities', 'Wholesale_Trade', 'Retail_Trade', 'Transportation|Warehousing|Utilities', 'Financial_Activities', 'Professional_and_Business_Services', 'Education|Health_Services', 'Leisure_and_Hospitality', 'Other_Services', 'Government'];
                         predictedVariable = target_variable;
                         dataItemName = "Data Entries";
                         populateFeatureSelection(features);
